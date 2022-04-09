ebf599c2420e14aa612e0075d13033091065ba4f
Alexander Nilges <nilges.alex@gmx.de>
2022-04-09 12:28:04 +0200

    completed Bird Watcher

---
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        if (birdsPerDay.length == 0) return 0;
+        else {
+            return birdsPerDay[birdsPerDay.length-1];
+        }
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length-1] += 1;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        int[] sorted = birdsPerDay;
+        Arrays.sort(sorted);
+        if (sorted[0] == 0) return true;
+        else return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int num = 0;
+        for (int i = 0; i < numberOfDays && i < birdsPerDay.length; i++) {
+            num += birdsPerDay[i];
+        }
+        return num;
+    }
+
+    public int getBusyDays() {
+        int busyDays = 0;
+        for (int bPD: birdsPerDay) {
+            if (bPD >= 5) {
+                busyDays += 1;
+            }
+        }
+        return busyDays;
+    }
+}
